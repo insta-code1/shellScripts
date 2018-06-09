e524033ea2d9ef64f3739eb4b83d12e7d0f6ae01
insta-code1 <eoinkane1@gmail.com>
2018-06-09 14:42:44 +0100

    Added command line arguments

---
+++ localusers/luser-demo06.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+
+# This file generates a random password for each user specified on the command line.
+
+# Display what the user typed on the command line.
+echo "You executed this command: ${0}"
+
+# Display the path and filename of this script.
+echo "You used $(dirname ${0}) as the path to execute this $(basename ${0}) script."
+
+# Tell them how many commands wear passed in.
+# (Inside the script they are parameters, outside they are arguments.)
+NUMBER_OF_PARAMETERS="${#}"
+echo "You supplied ${NUMBER_OF_PARAMETERS} argument(s) on the command line."
+
+# Make sure the supplied at least on argument.
+if [[ "${NUMBER_OF_PARAMETERS}" -lt 1 ]]
+then 
+    echo  "Usage: ${0} USER_NAME [USER_NAME]..."
+    exit 1
+fi    

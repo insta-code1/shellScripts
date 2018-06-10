78553d37ada70323849fcbc2aecd1197a06683e8
insta-code1 <eoinkane1@gmail.com>
2018-06-10 01:07:48 +0100

    Added script to work with shift and while loop.

---
+++ localusers/luser-demo07.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+
+# Demonstrate how to use shift and while loops.
+
+while [[ "${#}" -gt 0 ]]
+do
+    echo "Number of parameters: ${#}"
+    echo "Parameter 1: ${1}"
+    echo "Parameter 2: ${2}"
+    echo "Parameter 3: ${3}"
+    echo
+    shift 
+done

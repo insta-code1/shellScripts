bb5f749a2868d50f5503d626278c8cfe9dac30d8
insta-code1 <eoinkane1@gmail.com>
2018-06-21 23:20:15 +0100

    Work with output from server log files.

---
+++ localusers/luser-demo13.sh
@@ -0,0 +1,6 @@
+#¡/bin/bash
+
+# This port shows the open network ports on a system.
+# Use -4 as an argument to limit to tcpv4 ports.
+
+netstat -nutl ${1} | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}' 

1496a7f0c7ac65fec45ed9083b99008596e0c1e3
insta-code1 <eoinkane1@gmail.com>
2018-06-23 11:50:14 +0100

    Removing unnecessary comments.

--- localusers/show-attackers.sh
+++ localusers/show-attackers.sh
@@ -25,12 +25,3 @@ do
 	fi
 done
 exit 0
-
-
-
-
-# Display the CSV header.
-
-# Loop through the list of failed attempts and corresponding IP addresses.
-
- # If the number of failed attempts is greater than the limit, display count, IP, and location.
